1b3ca9f8befb410c54831521a766ac985438f4db
hyperschwartz <jschwartz@figure.com>
2022-01-28 16:33:25 -0800

    Give the sender admin access

--- src/helper.rs
+++ src/helper.rs
@@ -3,12 +3,15 @@ use provwasm_std::MarkerAccess;
 
 /// Global Variables
 pub const ONE_HUNDRED: Uint128 = Uint128::new(100);
-pub const SENDER_MARKER_PERMISSIONS: [MarkerAccess; 5] = [
+pub const SENDER_MARKER_PERMISSIONS: [MarkerAccess; 6] = [
     MarkerAccess::Burn,
     MarkerAccess::Deposit,
     MarkerAccess::Mint,
     MarkerAccess::Transfer,
     MarkerAccess::Withdraw,
+    // Administrator access is a requirement on the sender to ensure the wallet can sign for scope
+    // creation
+    MarkerAccess::Admin,
 ];
 pub const CONTRACT_MARKER_PERMISSIONS: [MarkerAccess; 7] = [
     MarkerAccess::Mint,
